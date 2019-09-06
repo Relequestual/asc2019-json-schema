4622d4958a675677bf2ae92d161d27b64bf8e0e2
Ben Hutton <bh7@sanger.ac.uk>
2019-09-06 16:17:51 +0100

    WIP

--- myTheme.js
+++ myTheme.js
@@ -1,10 +1,27 @@
+import theme from 'mdx-deck/themes'
+import { white } from 'ansi-colors';
+
 export default {
   styles: {
+    ...theme,
+    strong: {
+      color: 'black'
+    },
     'p': {
       'paddingLeft': '1em',
       'paddingRight': '1em',
+      '@media screen and (min-width: 64em)':{
+        fontSize: '32px',
+        textAlign: 'left',
+      }
     },
     h1: {
+      'textAlign': 'center',
+    },
+    h2: {
+      'textAlign': 'center',
+    },
+    h3: {
       'textAlign': 'center',
     },
     ul: {
@@ -13,11 +30,20 @@ export default {
     li: {
       marginBottom: '0.5em',
     },
+    // LIke githib
+    code: {
+      backgroundColor: 'rgba(27, 31, 35, 0.05)',
+      padding: '3px',
+      color: '#268bd2', //Solarized
+      borderRadius: '3px',
+      padding: '2px 5px',
+    },
     'Slide': {
-      // 'background-color': 'orange',
-    }
+      padding: '0',
+      display: 'flex',
+    },
   },
   colors: {
-    background: 'white'
-  }
+    background: '#f6f8fa'
+  },
 };
--- schemas/basic/index.js
+++ schemas/basic/index.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 const digits = 3;
 
-const fileNames = _.range(1, 3 + 1).map( num => num.toString().padStart(digits, '0'));
+const fileNames = _.range(1, 7 + 1).map( num => num.toString().padStart(digits, '0'));
 
 const files = fileNames.reduce((contents, file) => ({
   ...contents,
